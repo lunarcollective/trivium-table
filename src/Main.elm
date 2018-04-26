d2501d10eda4e5cab04ec55d183b4f66f9f481d1
Matt Rice <mattrice12@outlook.com>
2018-04-26 16:10:33 -0500

    added dropdown with checkboxes

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,6 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img, input)
+import Html exposing (Html, text, div, input, label)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (class, value, type_)
 
@@ -16,10 +16,10 @@ type SelectionType
 type alias State a =
     { id : String
     , query : String
-    , entries : Maybe (List Entry)
+    , entries : List Entry
     , selected : Maybe (List a)
     , open : Bool
-    , dropdownSelection : Maybe (List a)
+    , dropdownSelection : List String
     }
 
 
@@ -33,7 +33,7 @@ type alias Entry =
 
 init : ( State a, Cmd Msg )
 init =
-    ( State "" "" (Just initEntries) Nothing False Nothing, Cmd.none )
+    ( State "" "" initEntries Nothing False [], Cmd.none )
 
 
 initEntries : List Entry
@@ -55,7 +55,7 @@ initEntries =
 type Msg
     = NoOp
     | UpdateQuery String
-    | ShowDropdown
+    | Dropdown String
 
 
 update : Msg -> State a -> ( State a, Cmd Msg )
@@ -69,40 +69,62 @@ update msg state =
             , Cmd.none
             )
 
-        ShowDropdown ->
-            ( { state | open = not state.open }, Cmd.none )
+        Dropdown header ->
+            ( { state
+                | open = not state.open
+                , dropdownSelection = "All" :: List.map (headerToEntryField header) state.entries
+              }
+            , Cmd.none
+            )
 
-        -- case header of
-        --   "Name" ->
-        --
-        --   "Title" ->
-        --   "Location" ->
         _ ->
             ( state, Cmd.none )
 
 
-filterEntries : String -> Maybe (List Entry) -> Maybe (List Entry)
+headerToEntryField : String -> Entry -> String
+headerToEntryField header entry =
+    case header of
+        "Name" ->
+            entry.name
+
+        "Title" ->
+            entry.title
+
+        "Location" ->
+            entry.location
+
+        _ ->
+            ""
+
+
+setDropdown : State a -> ( State a, Cmd Msg )
+setDropdown state =
+    ( { state
+        | open = not state.open
+        , dropdownSelection = List.map (\entry -> entry.name) state.entries
+      }
+    , Cmd.none
+    )
+
+
+filterEntries : String -> List Entry -> List Entry
 filterEntries query entries =
-    Maybe.map
-        (\list ->
-            List.map
-                (\entry ->
-                    let
-                        queryInName =
-                            String.contains (String.toLower query) (String.toLower entry.name)
+    List.map
+        (\entry ->
+            let
+                queryInName =
+                    String.contains (String.toLower query) (String.toLower entry.name)
 
-                        queryInTitle =
-                            String.contains (String.toLower query) (String.toLower entry.title)
+                queryInTitle =
+                    String.contains (String.toLower query) (String.toLower entry.title)
 
-                        queryInLocation =
-                            String.contains (String.toLower query) (String.toLower entry.location)
-                    in
-                        if queryInName || queryInTitle || queryInLocation then
-                            { entry | selectionType = Include }
-                        else
-                            { entry | selectionType = Exclude }
-                )
-                list
+                queryInLocation =
+                    String.contains (String.toLower query) (String.toLower entry.location)
+            in
+                if queryInName || queryInTitle || queryInLocation then
+                    { entry | selectionType = Include }
+                else
+                    { entry | selectionType = Exclude }
         )
         entries
 
@@ -119,34 +141,26 @@ view state =
             , div [ class "query-text" ] [ text state.query ]
             ]
         , div [ class "table__container" ] <| showEntries state
-        , if state.open then
-            div [] [ text "OH MY GOD" ]
-          else
-            text ""
+        , div [] [ showDropdown state ]
         ]
 
 
 showEntries : State a -> List (Html Msg)
 showEntries state =
-    case state.entries of
-        Just entries ->
-            let
-                filteredEntries =
-                    List.filter (\entry -> entry.selectionType == Include) entries
-            in
-                case filteredEntries of
-                    [] ->
-                        [ div [ class "table" ] [ text "No Matches" ] ]
+    let
+        filteredEntries =
+            List.filter (\entry -> entry.selectionType == Include) state.entries
+    in
+        case filteredEntries of
+            [] ->
+                [ div [ class "table" ] [ text "No Matches" ] ]
 
-                    _ ->
-                        [ div [ class "table" ]
-                            [ div [ class "table__row headers" ] <| tableHeaders
-                            , div [ class "table__body" ] <| tableBody filteredEntries
-                            ]
-                        ]
-
-        Nothing ->
-            [ div [ class "table" ] [ text "No List" ] ]
+            _ ->
+                [ div [ class "table" ]
+                    [ div [ class "table__row headers" ] <| tableHeaders
+                    , div [ class "table__body" ] <| tableBody filteredEntries
+                    ]
+                ]
 
 
 tableHeaders : List (Html Msg)
@@ -159,7 +173,7 @@ tableHeaders =
             (\header ->
                 div [ class "table__entry-block flex" ]
                     [ div [ class "text" ] [ text header ]
-                    , div [ class "drop-down", onClick ShowDropdown ] [ text "^" ]
+                    , div [ class "drop-down", onClick (Dropdown header) ] [ text "^" ]
                     ]
             )
             headers
@@ -176,6 +190,22 @@ tableBody entries =
                 ]
         )
         entries
+
+
+showDropdown : State a -> Html Msg
+showDropdown state =
+    if state.open then
+        div [ class "dropdown-list" ] <|
+            List.map
+                (\item ->
+                    label []
+                        [ input [ type_ "checkbox" ] []
+                        , text item
+                        ]
+                )
+                state.dropdownSelection
+    else
+        text ""
 
 
 
